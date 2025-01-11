9680ff59df654b4fafa2d4925fb0aa519024a619
Tristan Graaff <tristangraaff@yahoo.com>
2025-01-11 18:06:04 +0100

    Added some test assertions

--- 02-basics-starting-project/01 Starting Project/cypress/e2e/basics.cy.js
+++ 02-basics-starting-project/01 Starting Project/cypress/e2e/basics.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+/// <reference types="Cypress" />
+
+describe('tasks page', () => {
+  it('should render the main image', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('img');
+  });
+
+  it('should render the h1 element and show the correct expected text', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('h1')
+      .should('be.visible')
+      .and('have.text', 'React Tasks');
   });
 });
