ca394a465464a181831fe268d80bcf352983e6b9
Tristan Graaff <tristangraaff@yahoo.com>
2025-01-14 17:08:38 +0100

    added 'page navigation' test case

--- 03-diving-deeper-starting-project/01 Starting Project/cypress/e2e/navigation.cy.js
+++ 03-diving-deeper-starting-project/01 Starting Project/cypress/e2e/navigation.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('page navigation', () => {
+  it('should navigate between pages', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('[data-cy=header-about-link]').click();
+    cy.location('pathname').should('eq', '/about');
+    cy.go('back');
+    cy.location('pathname').should('eq', '/');
+    cy.get('[data-cy=header-about-link]').click();
+    cy.get('[data-cy=header-home-link]').click();
+    cy.location('pathname').should('eq', '/');
   });
 });
