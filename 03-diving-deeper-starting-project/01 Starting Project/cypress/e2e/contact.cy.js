c9dc2dd062d11c436351300e9311f044bbc796b6
Tristan Graaff <tristangraaff@yahoo.com>
2025-01-15 17:49:47 +0100

    finished 'should validate form input' test case

--- 03-diving-deeper-starting-project/01 Starting Project/cypress/e2e/contact.cy.js
+++ 03-diving-deeper-starting-project/01 Starting Project/cypress/e2e/contact.cy.js
@@ -5,21 +5,52 @@ describe('contact form', () => {
         cy.visit('http://localhost:5173/about');
         cy.get('[data-cy="contact-input-message"]').type('Hello World');
         cy.get('[data-cy="contact-input-name"]').type('Tristan');
-        cy.get('[data-cy="contact-input-email"]').type('dummymail@yourmail.com');
         cy.get('[data-cy="contact-btn-submit"]').then((el) => {
             expect(el.attr('disabled')).to.be.undefined;
             expect(el.text()).to.eq('Send Message');
         });
-
-        // cy.get('[data-cy=contact-btn-submit]').as('submitBtn');
+        
         // cy.get('@submitBtn')
         //     .contains('Send Message')
         //     .and('not.have.attr', 'disabled'); 
         // Dit is de standaard Cypress manier. Hierboven staand met .then is een alternatief hierop
 
-        cy.get('@submitBtn').click();
+        cy.get('[data-cy="contact-input-email"]').type('dummymail@yourmail.com{enter}');
+        cy.get('[data-cy=contact-btn-submit]').as('submitBtn');
         cy.get('@submitBtn')
             .contains('Sending...')
             .should('have.attr', 'disabled');
+    });
+
+    it('should validate the form input', () => {
+        cy.visit('http://localhost:5173/about');
+        cy.get('[data-cy=contact-btn-submit]').as('submitBtn');
+        cy.get('@submitBtn').click();
+        cy.get('@submitBtn').then(el => {
+            expect(el).to.not.have.attr('disabled');
+            expect(el.text()).to.not.eq('Sending...');
+        });
+
+        cy.get('@submitBtn').contains('Send Message');
+        cy.get('[data-cy="contact-input-message"]').as('msgInput');
+        cy.get('@msgInput').focus().blur();
+        cy.get('@msgInput')
+            .parent()
+            .invoke('attr', 'class')
+            .should('include', 'invalid');
+
+        cy.get('[data-cy="contact-input-name"]').as('nameInput');
+        cy.get('@nameInput').focus().blur();
+        cy.get('@nameInput')
+            .parent()
+            .should('have.attr', 'class')
+            .and('match', /invalid/); //alternatief op regel 37
+
+        cy.get('[data-cy="contact-input-name"]').as('emailInput');
+        cy.get('@emailInput').focus().blur();
+        cy.get('@emailInput')
+            .parent()
+            .invoke('attr', 'class')
+            .should('include', 'invalid');
     });
 });
